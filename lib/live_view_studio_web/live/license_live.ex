b7bc4a70243ba0d8091970dc6e6080d7b79a4c44
Keisha Gittens <93796800+kgittens89@users.noreply.github.com>
2022-08-25 16:31:44 -0500

    Add countdown timer

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -5,12 +5,25 @@ defmodule LiveViewStudioWeb.LicenseLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 3, amount: Licenses.calculate(3))
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :message)
+    end
+
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
+    socket =
+      assign(socket,
+      seats: 3,
+      amount: Licenses.calculate(3),
+      expiration_time: expiration_time,
+      time_remaining: time_remaining(expiration_time),
+      mess: ""
+      )
     {:ok, socket}
   end
 
   def render(assigns) do
-    ~L"""
+    ~H"""
     <h1>Team License</h1>
     <div id="license">
       <div class="card">
@@ -25,12 +38,19 @@ defmodule LiveViewStudioWeb.LicenseLive do
 
           <form phx-change="update">
             <input type="range" min="1" max="10"
-                  name="seats" value="<%= @seats %>" />
+                  name="seats" value=" {@seats} " />
           </form>
 
           <div class="amount">
             <%= number_to_currency(@amount) %>
           </div>
+            <p>
+              <%= if @time_remaining >= 0  do %>
+                <%= format_time(@time_remaining) %>
+              <% else %>
+                Expired!!
+              <% end %>
+            </p>
         </div>
       </div>
     </div>
@@ -47,5 +67,24 @@ defmodule LiveViewStudioWeb.LicenseLive do
       )
 
     {:noreply, socket}
+  end
+
+  def handle_info(:message, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket =
+      assign(socket,
+        time_remaining: time_remaining(expiration_time)
+      )
+      {:noreply, socket}
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
